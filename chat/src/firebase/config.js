bd2890ece686cf0238569e537afb1b061f47301a
caothanh651998 <caothanh651998@gmail.com>
2021-06-11 19:36:05 +0700

    up code

---
+++ chat/src/firebase/config.js
@@ -0,0 +1,10 @@
+import Firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArYd4aQi3HOa766VMlZnvV6Ve_akqAPbw",
+  databaseURL: "https://rnfb-49fde-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "rnfb-49fde",
+  appId: "1: 743149160918: android: 04207ecf95bc743a89bc77",
+};
+
+export default Firebase.initializeApp(firebaseConfig);
